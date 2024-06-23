3d0369a6ac5107edeef34ef77827f96e48612443
gouravkrosx <gouravgreatkr@gmail.com>
2024-06-23 20:30:03 +0530

    fix: running script
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>

--- .github/workflows/test_workflows/keploy-linux.sh
+++ .github/workflows/test_workflows/keploy-linux.sh
@@ -13,7 +13,7 @@ curl --silent -o keployE --location https://keploy-enterprise.s3.us-west-2.amazo
 sudo chmod a+x keployE && sudo mkdir -p /usr/local/bin && sudo mv keployE /usr/local/bin
 
 
-sudo -E env PATH="$PATH" /usr/local/bin/keployE test -c "${GITHUB_WORKSPACE}/scripts/local_server.sh" --delay 40 --apiTimeout 300 --generateGithubActions=false
+sudo -E env PATH="$PATH" /usr/local/bin/keployE test -c "${GITHUB_WORKSPACE}/scripts/keploy_local_server.sh" --delay 40 --apiTimeout 300 --generateGithubActions=false
 echo "Keploy started in test mode"
 
 all_passed=true
